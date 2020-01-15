f05f8e105ca3585d46e4eacb7cd40efb441fbca8
PaperPlane01 <dagfortnewage@gmail.com>
2020-01-15 13:17:54 +0600

    fix: (15.01.20) Fixed handling of file uploading and payment status update in DdsApiClient

--- src/dds-api/DdsApiClient.ts
+++ src/dds-api/DdsApiClient.ts
@@ -1,7 +1,12 @@
-import {Injectable, Inject} from "@nestjs/common";
-import {AxiosInstance, AxiosPromise} from "axios";
+import {Inject, Injectable} from "@nestjs/common";
+import {AxiosError, AxiosInstance, AxiosPromise} from "axios";
 import {ExtendStorageDurationRequest, NotifyPaymentStatusRequest, UploadFileRequest} from "./types/request";
-import {DdsApiResponse, ExtendFileStorageDurationResponse, NotifyPaymentStatusResponse, UploadFileResponse} from "./types/response";
+import {
+    DdsApiResponse,
+    ExtendFileStorageDurationResponse,
+    NotifyPaymentStatusResponse,
+    UploadFileResponse
+} from "./types/response";
 import {wrapDdsApiRequest} from "./utils";
 import {DdsApiType} from "./types";
 
@@ -11,14 +16,32 @@ export class DdsApiClient {
     }
 
     public uploadFile(uploadFileRequest: UploadFileRequest): AxiosPromise<DdsApiResponse<UploadFileResponse>> {
-        return this.axios.post("/files", uploadFileRequest);
+        return new Promise((resolve, reject) => {
+            this.axios.post("/files", wrapDdsApiRequest(uploadFileRequest, DdsApiType.FILE))
+                .catch((error: AxiosError) => {
+                    if (error.response && error.response.status === 402) {
+                        resolve(error.response);
+                    } else {
+                        reject(error);
+                    }
+                })
+        });
     }
 
     public extendFileStorageDuration(
         fileId: string,
         extendStorageDurationRequest: ExtendStorageDurationRequest
     ): AxiosPromise<DdsApiResponse<ExtendFileStorageDurationResponse>> {
-        return this.axios.patch(`/files/${fileId}`, extendStorageDurationRequest);
+        return new Promise((resolve, reject) => {
+            this.axios.patch(`/files/${fileId}`, wrapDdsApiRequest(extendStorageDurationRequest, DdsApiType.FILE))
+                .catch((error: AxiosError) => {
+                    if (error.response && error.response.status === 402) {
+                        resolve(error.response);
+                    } else {
+                        reject(error);
+                    }
+                })
+        })
     }
 
     public getFile(fileId: string): AxiosPromise {
