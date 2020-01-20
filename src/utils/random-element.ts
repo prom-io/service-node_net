774f9a069fcb65d276296aacc854ab9dff929bfd
PaperPlane01 <dagfortnewage@gmail.com>
2020-01-20 13:01:15 +0600

    fix: (20.01.20) Fixed getRandomElement() function returning undefined elements

--- src/utils/random-element.ts
+++ src/utils/random-element.ts
@@ -1,4 +1,4 @@
 export const getRandomElement = <Type>(array: Type[]): Type => {
-    const randomIndex = Math.floor(Math.random() * (array.length + 1));
+    const randomIndex = Math.floor(Math.random() * array.length);
     return array[randomIndex];
 };
